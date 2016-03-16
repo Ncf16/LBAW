2bcd95acd49e1294555cd841aaf11cfec7bf1a2a
pedromlcosta <pedromlcosta@outlook.pt>
2016-03-16 21:35:50 +0000

    unitPage

--- Product/index.php
+++ Product/index.php
@@ -1,105 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include_once("templates/header.html");
+?>
 
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Academic Management</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/custom_style.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header navbar-right">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#CollapsibleMenu">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-
-                <ul class="nav navbar-nav navbar-left" style="max-width:150px;">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Teacher/Student<b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="profile.html">Profile</a>
-                            </li>
-                            <li>
-                                <a href="request.html">Request</a>
-                            </li>
-                            <li>
-                                <a href="index.html">Logout</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-                 
-                 
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="CollapsibleMenu">
-                <ul class="nav navbar-nav navbar-left">
-                  
-                	<li class="nav-brand">
-                		<a href="index.html">Home</a>
-                	</li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Courses<b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="informatica.html">Engenharia Informática</a>
-                            </li>
-                            <li>
-                                <a href="eletrotecnica.html">Engenharia Eletrotécnica</a>
-                            </li>
-                            <li>
-                                <a href="quimica.html">Engenharia Química</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-brand">
-                		<a href="evaluations.html">Evaluations</a>
-                	</li>
-                    
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-        <!-- jQuery -->
-<script src="js/jquery.js"></script>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="js/bootstrap.min.js"></script>
 
     <!-- Header Carousel -->
     <header id="myCarousel" class="carousel slide">
@@ -153,7 +55,7 @@
             </div>
             <div class="col-md-4">
                 <div class="panel panel-default">
-                	<a href="#"  >
+                	<a href="unitPage.php"  >
                     	<div class="panel-heading text-center btn-header">
                         	<h3> <b>LBAW</b></h3>
                     	</div>
@@ -169,7 +71,7 @@
             </div>
             <div class="col-md-4">
                 <div class="panel panel-default ">
-                    <a href="#"  >
+                    <a href="unitPage.php"  >
                     	<div class="panel-heading text-center btn-header">
                         	<h3> <b>LBAW</b></h3>
                     	</div>
@@ -185,7 +87,7 @@
             </div>
             <div class="col-md-4">
                 <div class="panel panel-default">
-                    <a href="#"  >
+                    <a href="unitPage.php"  >
                     	<div class="panel-heading text-center btn-header">
                         	<h3> <b>PPIN</b></h3>
                     	</div>
---
+++ Product/unitPage.php
@@ -0,0 +1,177 @@
+<?php
+include_once("templates/header.html");
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12">
+      <h2 class="page-header">Laboratório de Base de Dados e Aplicações Web</h2>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-5 col-md-offset-1 ">
+      <div class="text-center">
+        <h2>Information</h2>
+      </div>
+      <table class="table table-responsive ucInfo">
+          <tr>
+            <td>Sigla: LBAW</td>
+          </tr>
+          <tr>
+            <td>Regent: <a href="#">João Correia</a></td>
+          </tr>
+          <tr>
+            <td>Página: <a href="http://web.fe.up.pt/~jlopes/doku.php/teach/lbaw/index">http://web.fe.up.pt/~jlopes/doku.php/teach/lbaw/index</a> </td>
+          </tr>
+          <tr>
+            <td>Área: Desenvolvimento Web e Sistemas de Informação</td>
+          </tr>
+          <tr>
+            <td>Lorem: Ipsum</td>
+          </tr>
+       </table>
+    </div>
+    <div class="text-center">
+      <div class="col-md-5">
+        <h2>Teaching</h2>
+      </div>
+      <!-- teachers table-->
+      <table  class="ucInfo" cellpadding="10">
+        <tr>
+          <th>Type</th>
+          <th>Teacher</th>
+          <th>Classes</th>
+          <th>Hour</th>
+        </tr>
+        <tr class="d">
+          <td rowspan="3" class="k t"><h4> Lectures </h4></td>
+          <td class="k t">Totals</td>
+          <td class="k n">1</td>
+          <td class="k n">   2,00</td>
+          <tr class="d">
+          <td class="t"><a href="func_geral.formview?p_codigo=230756">João António Correia Lopes</a></td>
+          <td class="t"></td>
+          <td class="n">   1,00</td>
+        </tr>
+        <tr class="d">
+          <td class="t"><a href="func_geral.formview?p_codigo=310021">Sérgio Sobral Nunes</a></td>
+          <td class="t"></td>
+          <td class="n">   1,00</td>
+        </tr>
+
+        <tr class="d">
+          <td rowspan="4" class="k t"><h4> Laboratory Practice </h4></td>
+          <td class="k t">Totals</td>
+          <td class="k n">6</td>
+          <td class="k n">  18,00</td>
+        </tr>
+        <tr class="d">
+          <td class="t"><a href="func_geral.formview?p_codigo=230756">João António Correia Lopes</a></td>
+          <td class="t"></td>
+          <td class="n">   6,00</td>
+        </tr>
+        <tr class="d">
+          <td class="t"><a href="func_geral.formview?p_codigo=310021">Sérgio Sobral Nunes</a></td>
+          <td class="t"></td>
+          <td class="n">   6,00</td>
+        </tr>
+        <tr class="d">
+            <td class="t"><a href="func_geral.formview?p_codigo=479881">Tiago Boldt Pereira de Sousa</a></td>
+            <td class="t"></td>
+            <td class="n">   6,00</td>
+        </tr>
+      </table>
+      <!-- teachers table end-->
+    </div>
+  </div>
+
+  <p>
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>Syllabus</h2>
+      </div>
+    </div>
+  </p>
+
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="panel-group" id="accordion">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#year1">1 Year</a>
+            </h4>
+          </div>
+          <div id="year1" class="panel-collapse collapse">
+            <div class="panel-body">
+              <h4 class="panel-title">Heading</h4>
+              <div class="panel-group" id="accordion21">
+                <div class="panel">
+                  <a data-toggle="collapse" data-parent="#accordion21" href="#collapseTwoOne">View details 2.1 »</a>
+                  <div id="collapseTwoOne" class="panel-collapse collapse">
+                    <div class="panel-body">Details 1</div>
+                  </div>
+                </div>
+                <div class="panel ">
+                  <a data-toggle="collapse" data-parent="#accordion21" href="#collapseTwoTwo">View details 2.2 »</a>
+                  <div id="collapseTwoTwo" class="panel-collapse collapse">
+                    <div class="panel-body">Details 2</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#year2">2 Year</a>
+            </h4>
+          </div>
+          <div id="year2" class="panel-collapse collapse">
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#year3">3 Year</a>
+            </h4>
+          </div>
+          <div id="year3" class="panel-collapse collapse">
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#year4">4 Year</a>
+            </h4>
+          </div>
+          <div id="year4" class="panel-collapse collapse">
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#year5">5 Year</a>
+            </h4>
+          </div>
+          <div id="year5" class="panel-collapse collapse">
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
