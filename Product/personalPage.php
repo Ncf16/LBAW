fad449159e6c1aa88980d0c6dfecf6c77f9230a8
Ncf16 <joaofilipe26@gmail.com>
2016-03-16 21:31:43 +0000

    Personal Page

---
+++ Product/personalPage.php
@@ -0,0 +1,100 @@
+<?php
+include_once("templates/header.html"); 
+?>
+<!-- Page Content -->
+    <div class="container">
+
+        <!-- Portfolio Item Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Perosonal Page
+                    <small>Student's Name</small>
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Item Row -->
+        <div class="row">
+
+            <div class="col-md-3">
+                <img class="img-responsive" src="images/Students/.jpg" alt="studentImg"> <!--  src="http://placehold.it/750x500"-->
+            </div>
+
+            <div class="col-md-2">
+                <h3>Course</h3>
+               <a href="CoursePage_MIEIC.html">MIEIC</a>
+                 </div>
+
+                <div class="col-md-3">
+                <h3>Personal Detials</h3>	
+                <ul>
+                    <li>Current Year: 3</li>
+                    <li>Starting Year: 2013</li>
+                    <li>Mobile Phone: 9XXXXXXXX</li>
+                    <li>Current Status: Valid</li>
+                </ul>
+            </div>
+	 <div class="col-md-4" >
+
+
+    <div class="panel-group" id="accordion">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseBox">Sociodemographic Data</a>
+                </h4>
+            </div>
+            <div id="collapseBox" class="panel-collapse collapse in">
+                <div class="panel-body">
+
+                </div>
+            </div>
+        </div>
+        </div>
+        
+      </div>
+        <!-- /.row                <h3>Sociodemographic data </h3>	 -->
+
+        <!-- Related Projects Row -->
+        <div class="row">
+
+            <div class="col-lg-12">
+                <h3 class="page-header">Related Projects</h3>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+            <div class="col-sm-3 col-xs-6">
+                <a href="#">
+                    <img class="img-responsive portfolio-item" src="http://placehold.it/500x300" alt="">
+                </a>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+      
+    </div>
+
+</div>
+<?php
+include_once("templates/footer.html"); 
+?>
