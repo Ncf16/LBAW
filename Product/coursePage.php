741d8139fa659d3b64673f8e881a13c1c01ba0b1
ruiProg <ruifilipeosorio17@hotmail.com>
2016-03-16 18:48:59 +0000

    coursePage.php update

--- Product/coursePage.php
+++ Product/coursePage.php
@@ -3,44 +3,124 @@ include_once("templates/header.html");
 ?>
 
 <div class="container">
-  <h2>Accordion Example</h2>
-  <p><strong>Note:</strong> The <strong>data-parent</strong> attribute makes sure that all collapsible elements under the specified parent will be closed when one of the collapsible item is shown.</p>
-  <div class="panel-group" id="accordion">
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">Collapsible Group 1</a>
-        </h4>
-      </div>
-      <div id="collapse1" class="panel-collapse collapse in">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+  <div class="row">
+    <div class="col-lg-12">
+      <h2 class="page-header">Mestrado Integrado em Engenharia Informática</h2>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-8">
+      <h2>Description</h2>
+      <p>A master degree in Computer Science. In this curricular unit a student is expected to learn skills in order to manage networks, work in group projects, develop and mantain software.</p>
+    </div>
+    <div class="col-md-4">
+      <h2>Details</h2>
+      <p>
+        <span class="glyphicon glyphicon-user" aria-hidden="true"></span> Director: João Carlos Pascoal Faria
+      </p>
+      <p>
+        <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>Creation Date: 2006/2007
+      </p>
+      <p>
+        <span class="glyphicon glyphicon-time" aria-hidden="true"></span> Duration: 5 years
+      </p>
+      <p>
+        <span class="glyphicon glyphicon-star" aria-hidden="true"></span>Academic Degree: Master
+      </p>
+       <p>
+        <span class="glyphicon glyphicon-education" aria-hidden="true"></span> Number of Students: 120
+      </p>
+
+    </div>
+  </div>
+
+  <p>
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>Syllabus</h2>
       </div>
     </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">Collapsible Group 2</a>
-        </h4>
-      </div>
-      <div id="collapse2" class="panel-collapse collapse">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+  </p>
+
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="panel-group" id="accordion">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#year1">1 Year</a>
+            </h4>
+          </div>
+          <div id="year1" class="panel-collapse collapse">
+            <div class="panel-body">
+              <h4 class="panel-title">Heading</h4>
+              <div class="panel-group" id="accordion21">
+                <div class="panel">
+                  <a data-toggle="collapse" data-parent="#accordion21" href="#collapseTwoOne">View details 2.1 »</a>
+                  <div id="collapseTwoOne" class="panel-collapse collapse">
+                    <div class="panel-body">Details 1</div>
+                  </div>
+                </div>
+                <div class="panel ">
+                  <a data-toggle="collapse" data-parent="#accordion21" href="#collapseTwoTwo">View details 2.2 »</a>
+                  <div id="collapseTwoTwo" class="panel-collapse collapse">
+                    <div class="panel-body">Details 2</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#year2">2 Year</a>
+            </h4>
+          </div>
+          <div id="year2" class="panel-collapse collapse">
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#year3">3 Year</a>
+            </h4>
+          </div>
+          <div id="year3" class="panel-collapse collapse">
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#year4">4 Year</a>
+            </h4>
+          </div>
+          <div id="year4" class="panel-collapse collapse">
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+          </div>
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4 class="panel-title">
+              <a data-toggle="collapse" data-parent="#accordion" href="#year5">5 Year</a>
+            </h4>
+          </div>
+          <div id="year5" class="panel-collapse collapse">
+            <div class="panel-body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">Collapsible Group 3</a>
-        </h4>
-      </div>
-      <div id="collapse3" class="panel-collapse collapse">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-      </div>
-    </div>
-  </div> 
+  </div>
 </div>
