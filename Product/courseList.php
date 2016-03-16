f7f39ff13f12deb2a378648efe639473c8074d93
Ncf16 <joaofilipe26@gmail.com>
2016-03-16 18:11:27 +0000

    Course List page, added css for the table

--- Product/courseList.php
+++ Product/courseList.php
@@ -1,49 +1,76 @@
+ 
 <?php
 include_once("templates/header.html"); 
 ?>
+<img src="images/books.jpg" class="img-responsive" alt="CourseBooks">
+ 
+<link href="css/courseList.css" rel="stylesheet">
 
-<table class="table table-reflow">
+<table class="table table-striped">
   <thead>
-    <tr>
-      <th>#</th>
-      <th>Table heading</th>
-      <th>Table heading</th>
-      <th>Table heading</th>
-      <th>Table heading</th>
-      <th>Table heading</th>
-      <th>Table heading</th>
+    <tr class="head">
+      <th class="text-center">Courses</th>
+      <th class="text-center">Director</th>
+      <th class="text-center">Creation Date</th>
+      <th class="text-center">Duration (years)</th>
+	  <th class="text-center">Academic Degree </th>
+      <th class="text-center">Number Of Students</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
+      <th   scope="row"><a href="CoursePage_MIEIC.html">MIEIC</a> </th>
+      <td  ><a href="Teacher_JPF.html">João Carlos Pascoal Faria</a> </td>
+      <td>2006/2007</td>
+      <td>5</td>
+      <td>Master</td>
+      <td>585</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
+     <th scope="row"><a href="CoursePage_CINF.html">CINF</a> </th>
+      <td><a href="Teacher_OMMP.html">Olívia Pestana</a> </td>
+      <td>2007/2008</td>
+      <td>3</td>
+      <td>Bachelor</td> 
+      <td>120</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
-      <td>Table cell</td>
+      <th scope="row"><a href="CoursePage_MIEM.html">MIEM</a> </th>
+      <td><a href="Teacher_LFS.html">Lucas Filipe Silva</a></td>
+      <td>2006/2007</td>
+      <td>5</td>
+      <td>Master</td>
+      <td>805</td>
+    </tr>
+     <tr>
+      <th scope="row"><a href="CoursePage_MIEM.html">MIEM</a> </th>
+      <td><a href="Teacher_LFS.html">Lucas Filipe Silva</a></td>
+      <td>2006/2007</td>
+      <td>5</td>
+      <td>Master</td>
+      <td>805</td>
+    </tr>    
+    <tr>
+      <th scope="row"><a href="CoursePage_MIEM.html">MIEM</a> </th>
+      <td><a href="Teacher_LFS.html">Lucas Filipe Silva</a></td>
+      <td>2006/2007</td>
+      <td>5</td>
+      <td>Master</td>
+      <td>805</td>
+    </tr>
+       <tr>
+     <th scope="row"><a href="CoursePage_CINF.html">CINF</a> </th>
+      <td><a href="Teacher_OMMP.html">Olívia Pestana</a> </td>
+      <td>2007/2008</td>
+      <td>3</td>
+      <td>Bachelor</td> 
+      <td>120</td>
     </tr>
   </tbody>
 </table>
+<p>
+	A course may take up to 3 years if it is a Bachelor or 5 years if it is a Master. Futhermore each course contains multiple curricular units grouped by year.
+</p>
 <?php
 include_once("templates/footer.html"); 
 ?>
