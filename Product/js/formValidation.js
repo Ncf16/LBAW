5c90e06ea4681f3c963ded349832cb3c43a59fbc
ruiProg <ruifilipeosorio17@hotmail.com>
2016-03-25 19:53:16 +0000

    unitCreation done

--- Product/js/formValidation.js
+++ Product/js/formValidation.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
     $('#newMeth').on('click', addMethods);
-    $('#newProf').on('click', addMethods);
 });
 
 function addMethods(){
@@ -12,9 +11,32 @@ function addMethods(){
 	classesMethod(newMethod);
 	hoursMethod(newMethod);
 
-	var buttonProfessor = $('<button/>').attr('type','button').addClass('btn btn-default').attr('id','newProf').text('Add New Professor');
-	newMethod.append(buttonProfessor);
+	var buttonTeacher = $('<button/>',{
+		id: 'newProf',
+		type: 'button',
+		class: 'btn btn-default',
+		text: 'Add New Teacher'
+	});
+	newMethod.append(buttonTeacher);
+	newMethod.append($('<br>'));
 	$('#methods').append(newMethod);
+
+	$('div.panel').on('click', '#newProf', addTeacher);
+}
+
+function addTeacher(){
+	root = $(event.target).parent();
+	var parallelCols = $('<div/>').addClass('col-md-4');
+	var label = $('<label/>').addClass('col-md-2 control-label').text('Teacher');
+	var container = $('<div/>').addClass('col-md-9 inputGroupContainer');
+	var inputGroup = $('<div/>').addClass('input-group').append($('<span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>'));
+
+	container.append(inputGroup);
+	parallelCols.append(label);
+	parallelCols.append(container);
+
+	inputGroup.append($('<input name="unit_prof" placeholder="Name of teacher" class="form-control" type="text">'));
+	root.append(parallelCols);
 }
 
 function typeMethod(root){
