874ec9bd177eed147ffd1e67fae7302238c210fc
ruiProg <ruifilipeosorio17@hotmail.com>
2016-04-14 00:14:36 +0100

    list of courses

--- queries.sql
+++ queries.sql
@@ -6,13 +6,29 @@ SELECT
   curricularunitoccurrence.curricularyear, 
   curricularunitoccurrence.curricularsemester
 FROM 
-  public.syllabus, 
-  public.course, 
-  public.curricularunitoccurrence, 
-  public.curricularunit
+  syllabus, 
+  course, 
+  curricularunitoccurrence, 
+  curricularunit
 WHERE 
   syllabus.syllabusid = curricularunitoccurrence.syllabusid AND
   course.code = syllabus.coursecode AND
   course.currentcalendaryear = syllabus.calendaryear AND
   curricularunitoccurrence.curricularunitid = curricularunit.curricularid AND
   course.name = 'Chemistry';
+
+
+
+--List of courses
+SELECT 
+  course.name,course.description,course.creationDate,person.name,COUNT(courseenrollment.studentcode),course.courseType
+FROM 
+  courseenrollment, 
+  course,
+  person
+WHERE 
+  courseenrollment.courseid = course.code AND
+  courseenrollment.finishyear IS NOT NULL AND
+  course.teachercode = person.academiccode
+GROUP BY course.code,person.name;
+GROUP BY course.code,person.name;
