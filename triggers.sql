d05c1099dabe51e04682235139059e8bbeb3b380
ruiProg <ruifilipeosorio17@hotmail.com>
2016-04-13 17:23:22 +0100

    triggers working

--- triggers.sql
+++ triggers.sql
@@ -1,23 +1,214 @@
-DROP TRIGGER  IF EXISTS checkDiretorType ON Course CASCADE;
-DROP TRIGGER  IF EXISTS checkRegentType  ON CurricularUnitOccurrence CASCADE;
-DROP TRIGGER  IF EXISTS oneExamPerUC ON Exam CASCADE;
-DROP TRIGGER  IF EXISTS checkStudentType  ON Request CASCADE;
-DROP TRIGGER  IF EXISTS checkAdminType ON Request CASCADE;
-DROP TRIGGER  IF EXISTS checkRegentType  ON CurricularUnitOccurrence CASCADE;
-DROP TRIGGER  IF EXISTS checkStudentType ON Attendance CASCADE;
-DROP TRIGGER  IF EXISTS checkStudentType  ON Grade CASCADE;
-DROP TRIGGER  IF EXISTS checkStudentType ON Course CASCADE;
-DROP TRIGGER  IF EXISTS checkStudentType  ON CurricularEnrollment CASCADE;
-DROP TRIGGER  IF EXISTS checkStudentType ON CourseEnrollment CASCADE;
-DROP TRIGGER  IF EXISTS checkClassDate  ON Class CASCADE;
-DROP TRIGGER  IF EXISTS checkClassRoom  ON Class CASCADE;
-DROP TRIGGER  IF EXISTS checkCourseDate  ON CourseEnrollment CASCADE;
-DROP TRIGGER  IF EXISTS tsvectorPersonUpdate ON Person;
-DROP TRIGGER  IF EXISTS tsvectorCourseUpdate ON Course;
-DROP TRIGGER  IF EXISTS tsvectorCuUpdate ON CurricularUnit;
-DROP TRIGGER  IF EXISTS tsvectorCuUpdate ON Area;
+﻿DROP TRIGGER IF EXISTS checkDiretorType ON Course CASCADE;
+DROP TRIGGER IF EXISTS checkRegentType ON CurricularUnitOccurrence CASCADE;
+DROP TRIGGER IF EXISTS oneExamPerUC ON Exam CASCADE;
+DROP TRIGGER IF EXISTS checkStudentType ON Request CASCADE;
+DROP TRIGGER IF EXISTS checkAdminType ON Request CASCADE;
+DROP TRIGGER IF EXISTS checkRegentType ON CurricularUnitOccurrence CASCADE;
+DROP TRIGGER IF EXISTS checkStudentType ON Attendance CASCADE;
+DROP TRIGGER IF EXISTS checkStudentType ON Grade CASCADE;
+DROP TRIGGER IF EXISTS checkStudentType ON Course CASCADE;
+DROP TRIGGER IF EXISTS checkStudentType ON CurricularEnrollment CASCADE;
+DROP TRIGGER IF EXISTS checkStudentType ON CourseEnrollment CASCADE;
+DROP TRIGGER IF EXISTS checkClassDate ON Class CASCADE;
+DROP TRIGGER IF EXISTS checkClassRoom ON Class CASCADE;
+DROP TRIGGER IF EXISTS checkCourseDate ON CourseEnrollment CASCADE;
+DROP TRIGGER IF EXISTS tsvectorPersonUpdate ON Person;
+DROP TRIGGER IF EXISTS tsvectorCourseUpdate ON Course;
+DROP TRIGGER IF EXISTS tsvectorCuUpdate ON CurricularUnit;
+DROP TRIGGER IF EXISTS tsvectorCuUpdate ON Area;
+
+--Functions
+CREATE OR REPLACE FUNCTION getPersonType(id INTEGER) 
+RETURNS PersonType AS $$
+DECLARE
+result PersonType;
+BEGIN
+ SELECT PERSON.personType INTO result
+ FROM PERSON 
+ WHERE academicCode = id AND visible=1;
+return result; 
+END 
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION isPersonTeacher()
+RETURNS trigger AS $$
+DECLARE
+ type PersonType;
+BEGIN
+type:=getPersonType(NEW.teacherCode);
+ IF (type = 'Teacher' )
+ THEN 
+  RETURN NEW;
+  ELSE
+  RETURN NULL;-- RAISE EXCEPTION 'User is not a Teacher';
+ END IF;
+END 
+$$ LANGUAGE 'plpgsql'; 
+
+CREATE  OR REPLACE FUNCTION isPersonStudent()
+RETURNS trigger AS $$
+DECLARE
+ type PersonType;
+BEGIN
+type:=getPersonType(NEW.studentCode);
+ IF (type = 'Student' )
+ THEN 
+  RETURN NEW;
+  ELSE
+  RETURN NULL; -- RAISE EXCEPTION 'User is not a Student';
+ END IF;
+END 
+$$ LANGUAGE 'plpgsql'; 
+ 
+CREATE  OR REPLACE FUNCTION isPersonAdmin()
+RETURNS trigger AS $$
+DECLARE
+ type PersonType;
+BEGIN
+type:=getPersonType(NEW.adminCode);
+ IF (type = 'Admin' )
+ THEN 
+  RETURN NEW;
+  ELSE
+  RETURN NULL;-- RAISE EXCEPTION 'User is not an Admin';
+ END IF;
+END 
+$$ LANGUAGE 'plpgsql';
+
+
+CREATE OR REPLACE FUNCTION isClassDateValid()
+RETURNS trigger AS $$
+DECLARE
+ beginDate DATE;
+ endDate DATE;
+BEGIN
+SELECT calendar.begindate, calendar.enddate INTO beginDate, endDate
+FROM syllabus, curricularunitoccurrence, calendar
+WHERE 
+ curricularunitoccurrence.visible=1 AND calendar.visible=1 AND syllabus.visible=1 AND 
+ NEW.occurrenceid = curricularunitoccurrence.cuoccurrenceid AND
+ syllabus.calendaryear = calendar.year AND
+ curricularunitoccurrence.syllabusid = syllabus.syllabusid AND
+ curricularunitoccurrence.curricularsemester = calendar.semester;
+
+ IF (NEW.classDate::date BETWEEN beginDate AND endDate)
+ THEN RETURN NEW;
+ ELSE RETURN NULL;
+ END IF; 
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION isRoomAvailable()
+RETURNS trigger AS $$
+DECLARE
+count INTEGER;
+BEGIN
+SELECT COUNT(class.classid) INTO count
+FROM Class
+WHERE Class.visible=1 AND Class.roomid = NEW.roomid 
+AND (NEW.classDate, interval '1' minute * NEW.duration) OVERLAPS
+(Class.classDate, interval '1' minute * class.duration);
+
+IF (count = 0)
+THEN RETURN NEW;
+ELSE RETURN NULL;
+END IF;
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION isCourseAvailable()
+RETURNS trigger AS $$
+DECLARE
+creation DATE;
+BEGIN
+SELECT 
+ course.creationdate INTO creation
+FROM 
+ course
+WHERE 
+ course.code = NEW.courseid;
+
+IF (NEW.startYear >= creation)
+THEN RETURN NEW;
+ELSE RETURN NULL;
+END IF;
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION isPersonAdmin() RETURNS trigger AS $$
+DECLARE
+ type PersonType;
+BEGIN
+type:=getPersonType(NEW.adminCode);
+ IF (type = 'Admin' )
+ THEN 
+  RETURN NEW;
+  ELSE
+  RETURN NULL;-- RAISE EXCEPTION 'User is not an Admin';
+ END IF;
+END 
+$$ LANGUAGE 'plpgsql'; 
+
+CREATE OR REPLACE FUNCTION examsPerOccurrence(occurrenceID INTEGER) RETURNS SETOF INTEGER AS $$
+ BEGIN
+ RETURN QUERY SELECT *
+  FROM Evaluation,Exam 
+   WHERE Exam.visible=1 AND Evaluation.visible=1 AND Evaluation.evaluationID = Exam.evaluationID AND Evaluation.cuOccurrenceID=occurrenceID ;
+ END
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION onlyOneExam() RETURNS trigger AS $$
+ DECLARE
+ numberOfExams INTEGER;
+BEGIN
+ SELECT COUNT(*) INTO numberOfExams FROM examsPerOccurrence(NEW.occurrenceID);
+ IF(numberOfExams >= 1)
+  THEN
+   RETURN NULL; --RAISE EXCEPTION 'Only 1 exam per Occurrence is allowed';
+  ELSE
+   RETURN NEW;
+   END IF;
+END $$ LANGUAGE 'plpgsql';
+ 
+-- FULL TEXT SEARCH TRIGGERS AND FUNCTIONS
+
+-- SEARCH FUNCTIONS
+ 
+ 
+CREATE FUNCTION person_search_trigger() RETURNS trigger AS $$
+begin
+ new.tsv := to_tsvector(coalesce(new.name,''));
+ return new;
+end
+$$ LANGUAGE 'plpgsql';
 
  
+CREATE FUNCTION course_search_trigger() RETURNS trigger AS $$
+begin
+ new.tsv :=
+  setweight(to_tsvector(coalesce(new.name,'')), 'A') ||
+  setweight(to_tsvector(coalesce(new.description,'')), 'D');
+ return new;
+end
+$$ LANGUAGE 'plpgsql';
+
+CREATE FUNCTION cu_search_trigger() RETURNS trigger AS $$
+begin
+ new.tsv :=
+  to_tsvector(coalesce(new.name,''));
+ return new;
+end
+$$ LANGUAGE 'plpgsql';
+
+CREATE FUNCTION area_search_trigger() RETURNS trigger AS $$
+begin
+ new.tsv :=
+  to_tsvector(coalesce(new.area,''));
+ return new;
+end
+$$ LANGUAGE 'plpgsql';
+
+
 CREATE TRIGGER oneExamPerUC
 BEFORE INSERT ON Exam
 FOR EACH ROW
@@ -42,25 +233,27 @@ EXECUTE PROCEDURE isPersonAdmin();
 CREATE TRIGGER checkRegentType
 BEFORE INSERT OR UPDATE ON CurricularUnitOccurrence 
 FOR EACH ROW
-EXECUTE PROCEDURE  isPersonTeacher(); 
+EXECUTE PROCEDURE isPersonTeacher(); 
 
 CREATE TRIGGER checkStudentType
 BEFORE INSERT OR UPDATE ON Attendance 
 FOR EACH ROW
-EXECUTE PROCEDURE  isPersonStudent(); 
+EXECUTE PROCEDURE isPersonStudent();
+
 CREATE TRIGGER checkStudentType
 BEFORE INSERT OR UPDATE ON Grade 
 FOR EACH ROW
-EXECUTE PROCEDURE  isPersonStudent(); 
+EXECUTE PROCEDURE isPersonStudent(); 
+
 CREATE TRIGGER checkStudentType
 BEFORE INSERT OR UPDATE ON CurricularEnrollment 
 FOR EACH ROW
-EXECUTE PROCEDURE  isPersonStudent();
+EXECUTE PROCEDURE isPersonStudent();
 
 CREATE TRIGGER checkStudentType
 BEFORE INSERT OR UPDATE ON CourseEnrollment 
 FOR EACH ROW
-EXECUTE PROCEDURE  isPersonStudent();
+EXECUTE PROCEDURE isPersonStudent();
  
 
 CREATE TRIGGER checkClassDate
@@ -91,196 +284,3 @@ ON CurricularUnit FOR EACH ROW EXECUTE PROCEDURE cu_search_trigger();
 
 CREATE TRIGGER tsvectorCuUpdate BEFORE INSERT OR UPDATE
 ON Area FOR EACH ROW EXECUTE PROCEDURE area_search_trigger();
-
-
---Functions
-CREATE OR REPLACE FUNCTION getPersonType(id INTEGER) 
-RETURNS  PersonType AS  $$
-DECLARE
-result PersonType;
-BEGIN
-  SELECT PERSON.personType INTO result
-  FROM PERSON 
-  WHERE academicCode = id AND visible=1;
-return  result; 
-END 
-$$ LANGUAGE 'plpgsql';
- 
-
-
-CREATE OR REPLACE FUNCTION isPersonTeacher()
-RETURNS trigger AS  $$
-DECLARE
-  type PersonType;
-BEGIN
-type:=getPersonType(NEW.teacherCode);
-  IF (type =  'Teacher' )
-  THEN 
-    RETURN NEW;
-    ELSE
-   RETURN NULL;-- RAISE EXCEPTION 'User is not a Teacher';
-  END IF;
-END 
-$$  LANGUAGE 'plpgsql'; 
-
- CREATE OR REPLACE FUNCTION isPersonStudent()
-RETURNS trigger AS  $$
-DECLARE
-  type PersonType;
-BEGIN
-type:=getPersonType(NEW.studentCode);
-  IF (type =  'Student' )
-  THEN 
-    RETURN NEW;
-    ELSE
-   RETURN NULL; -- RAISE EXCEPTION 'User is not a Student';
-  END IF;
-END 
-$$  LANGUAGE 'plpgsql'; 
-  
-  CREATE OR REPLACE FUNCTION isPersonAdmin()
-RETURNS trigger AS  $$
-DECLARE
-  type PersonType;
-BEGIN
-type:=getPersonType(NEW.adminCode);
-  IF (type =  'Admin' )
-  THEN 
-    RETURN NEW;
-    ELSE
-   RETURN NULL;--  RAISE EXCEPTION 'User is not an Admin';
-  END IF;
-END 
-$$  LANGUAGE 'plpgsql';
-
-
-CREATE OR REPLACE FUNCTION isClassDateValid()
-RETURNS trigger AS $$
-DECLARE
- beginDate DATE;
- endDate DATE;
-BEGIN
-SELECT calendar.begindate, calendar.enddate INTO beginDate, endDate
-FROM syllabus, curricularunitoccurrence, calendar
-WHERE 
- curricularunitoccurrence.visible=1 AND calendar.visible=1 AND syllabus.visible=1 AND 
- NEW.occurrenceid = curricularunitoccurrence.cuoccurrenceid AND
-  syllabus.calendaryear = calendar.year AND
-  curricularunitoccurrence.syllabusid = syllabus.syllabusid AND
-  curricularunitoccurrence.curricularsemester = calendar.semester;
-
-  IF (NEW.classDate::date BETWEEN beginDate AND endDate)
-  THEN RETURN NEW;
-  ELSE RETURN NULL;
- END IF; 
-END
-$$ LANGUAGE 'plpgsql';
-
-CREATE OR REPLACE FUNCTION isRoomAvailable()
-RETURNS trigger AS $$
-DECLARE
-count INTEGER;
-BEGIN
-SELECT COUNT(class.classid) INTO count
-FROM Class
-WHERE Class.visible=1  AND  Class.roomid = NEW.roomid AND 
-AND (NEW.classDate, interval '1' minute * NEW.duration) OVERLAPS
-(Class.classDate, interval '1' minute * class.duration);
-
-IF (count = 0)
-THEN RETURN NEW;
-ELSE RETURN NULL;
-END IF;
-END
-$$ LANGUAGE 'plpgsql';
-
-CREATE OR REPLACE FUNCTION isCourseAvailable()
-RETURNS trigger AS $$
-DECLARE
-creation DATE;
-BEGIN
-SELECT 
-  course.creationdate INTO creation
-FROM 
-  course
-WHERE 
-  course.code = NEW.courseid;
-
-IF (NEW.startYear >= creation)
-THEN RETURN NEW;
-ELSE RETURN NULL;
-END IF;
-END
-$$ LANGUAGE 'plpgsql';
-
-CREATE OR REPLACE FUNCTION isPersonAdmin() RETURNS trigger AS  $$
-DECLARE
-  type PersonType;
-BEGIN
-type:=getPersonType(NEW.adminCode);
-  IF (type =  'Admin' )
-  THEN 
-    RETURN NEW;
-    ELSE
-   RETURN NULL;--  RAISE EXCEPTION 'User is not an Admin';
-  END IF;
-END 
-$$  LANGUAGE 'plpgsql';  
-
-CREATE OR REPLACE FUNCTION examsPerOccurrence(occurrenceID INTEGER) RETURNS SETOF INTEGER AS $$
- BEGIN
- RETURN QUERY SELECT *
-    FROM Evaluation,Exam 
-      WHERE Exam.visible=1 AND Evaluation.visible=1 AND Evaluation.evaluationID = Exam.evaluationID AND Evaluation.cuOccurrenceID=occurrenceID ;
-  END
-$$ LANGUAGE 'plpgsql';
-
-CREATE OR REPLACE FUNCTION onlyOneExam() RETURNS trigger AS $$
-  DECLARE
-  numberOfExams INTEGER;
-BEGIN
-  SELECT COUNT(*) INTO numberOfExams FROM examsPerOccurrence(NEW.occurrenceID);
-  IF(numberOfExams >= 1)
-    THEN
-      RETURN NULL; --RAISE EXCEPTION 'Only 1 exam per Occurrence is allowed';
-    ELSE
-      RETURN NEW;
-END  $$ LANGUAGE 'plpgsql';
- 
--- FULL TEXT SEARCH TRIGGERS AND FUNCTIONS
-
--- SEARCH FUNCTIONS
- 
- 
-CREATE FUNCTION person_search_trigger() RETURNS trigger AS $$
-begin
-  new.tsv := to_tsvector(coalesce(new.name,''));
-  return new;
-end
-$$ LANGUAGE 'plpgsql';
-
- 
-CREATE FUNCTION course_search_trigger() RETURNS trigger AS $$
-begin
-  new.tsv :=
-    setweight(to_tsvector(coalesce(new.name,'')), 'A') ||
-    setweight(to_tsvector(coalesce(new.description,'')), 'D');
-  return new;
-end
-$$ LANGUAGE 'plpgsql';
-
-CREATE FUNCTION cu_search_trigger() RETURNS trigger AS $$
-begin
-  new.tsv :=
-    to_tsvector(coalesce(new.name,''));
-  return new;
-end
-$$ LANGUAGE 'plpgsql';
-
-CREATE FUNCTION area_search_trigger() RETURNS trigger AS $$
-begin
-  new.tsv :=
-    to_tsvector(coalesce(new.area,''));
-  return new;
-end
-$$ LANGUAGE 'plpgsql';
