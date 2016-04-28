c51c1b6507d29661441a4e524e84cfa000f3240a
pedromlcosta <pedromlcosta@outlook.pt>
2016-04-28 03:36:23 +0100

    further login stuff

--- Product/api/login.php
+++ Product/api/login.php
@@ -2,14 +2,19 @@
   include_once('../config/init.php');
   include_once($BASE_DIR .'database/users.php');  
 
+  echo "kk";
+
   if (!$_POST['username'] || !$_POST['password']) {
 
-    return false;    
+    echo "false";    
     exit;
   }
 
   $username = $_POST['username'];
   $password = $_POST['password'];
+  
+  $message = isLoginCorrect($username, $password);
+  echo "hi";
   
   if (isLoginCorrect($username, $password)) {
     $_SESSION['username'] = $username;
--- Product/database/users.php
+++ Product/database/users.php
@@ -1,26 +1,23 @@
 <?php
-  
-  
+  include_once('../config/init.php');
+  require_once($BASE_DIR . "lib/password.php");
+
   function isLoginCorrect($username, $password){
   global $conn;
 
   try{
-  $query = 'SELECT * FROM USERS WHERE username = ?  COLLATE NOCASE';//   AND password = ?";
+  $query = 'SELECT * FROM PERSON WHERE LOWER(username) = LOWER(?) ';//   AND password = ?";
   $stmt = $conn->prepare($query);
   $stmt->execute(array($username));
   $user = $stmt->fetch();
-
-  if($stmt->fetch() != true){
-    return false;
-  }else{
-    $result=password_verify($password,$user['password']);
-  }
  
+  $result = password_verify($password, $user['password']);
+  
   // $result !== false means it found the user with the password
   if($result !== false){
-    return true;
+    return "2";//$user['username'];
   }else{
-    return false;
+    return "3";
   }
 
   }catch(PDOException $e){
@@ -30,7 +27,7 @@
     return false;
   }catch(Exception $e){
     echo "Unexpected Database Error: " . $e->getMessage();
-    return false;
+    return false
   }
 
 }
