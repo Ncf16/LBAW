9fefd240e2bf8ad79ce3d4509c16e8c21206b37f
Ncf16 <joaofilipe26@gmail.com>
2016-06-03 00:26:29 +0100

    check types

--- Product/api/personEdit.php
+++ Product/api/personEdit.php
@@ -2,7 +2,43 @@
 include_once('../config/init.php');
 include_once($BASE_DIR . 'database/person.php');
 
+checkArgs()
 $username=$_POST['username'];
 unset($_POST['username']);
 echo createUpdateQuery($_POST,$username,"username");
+
+    function checkArgs(){
+    	//Username
+    	if(!isset($_POST['username'])|| !is_numeric($_POST['username'])){
+    		echo "invalid user";
+    	} //Name
+    	else if{
+
+    	}  //Addr
+    	else if{
+
+    	} //Nationality
+    	else if{
+
+    	}//PhoneNumber
+    	else if{
+
+    	} //NIF
+   		 else if{
+
+    	} //BirthData
+   		 else if{
+
+    	}  //Password
+    	else if{
+
+    	}
+    
+   
+    
+     
+    
+    
+   
+    } 
 ?>
